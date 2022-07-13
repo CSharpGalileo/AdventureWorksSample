9ea2d8bfab5ade6ed137004d1a5822a8258f3b96
Abel Palma <apalma@tsc.com.gt>
2022-07-13 17:46:12 -0600

    Carga Inicial

---
+++ AdventureWorksLTDB/AdventureWorksLTDB/data/AdventureWorksContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventureWorksDBLT.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection; //IServiceCollection
+
+namespace AdventureWorksDBLT.data
+{
+    public static class AdventureWorksContextExtensions
+    {
+        public static IServiceCollection AddAdventureWorksDBContext(this IServiceCollection services, string strCnx = "Data Source=DESKTOP-S0NQNAG;Initial Catalog=AdventureWorksLT2019;Integrated Security=false;User=sa;Password=GalileoPrueba;")
+        {
+            services.AddDbContext<AdventureWorksDB>(options => options.UseSqlServer(strCnx));
+            return services;
+        }
+    }
+}
